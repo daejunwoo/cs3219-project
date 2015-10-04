549aa17154af30e7f77b4308a254555ca08046e8
daejunwoo <alan.tjw@gmail.com>
2015-10-04 11:04:02 +0800

    add directories

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from cStringIO import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run()
