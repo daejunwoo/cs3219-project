f1b476daef3e75233360b9a9939ff9e945db2a3d
wuchiasheng <wu.cecc@gmail.com>
2015-11-09 21:56:00 +0800

    integrate file upload and [conv,extract,analyze]

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask,request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.cors import CORS, cross_origin
 import os
+import shutil
 import convert as convertPDF
 import extract as ex
 import analyze as analyzer
@@ -18,8 +19,43 @@ db = SQLAlchemy(app)
 @app.route('/analyzer')
 def analyzeCV():
   description = {'Title': 'Software Engineer', 'Skill': ['Microsoft Office', 'Data Mining', 'Image Processing'], 'Certification': 'Random value', 'Volunteering': 'Random value'}
-  resume = [{'Name': 'Tom', 'Title': 'Software Engineer at NUS', 'Experience': [{'Title': 'Software Engineer'}], 'Skill': ['Microsoft Office', 'Data Mining'], 'Certification': 'Random value', 'Volunteering': 'Random value'}, {'Name': 'Sam', 'Title': 'Software Engineer at NUS', 'Experience': [{'Title': 'Software Engineer'}], 'Skill': ['Microsoft Office', 'Data Mining']}]
-  result = analyzer.process_analyzer(description, resume)
+  #resume = [{'Name': 'Tom', 'Title': 'Software Engineer at NUS', 'Experience': [{'Title': 'Software Engineer'}], 'Skill': ['Microsoft Office', 'Data Mining'], 'Certification': 'Random value', 'Volunteering': 'Random value'}, {'Name': 'Sam', 'Title': 'Software Engineer at NUS', 'Experience': [{'Title': 'Software Engineer'}], 'Skill': ['Microsoft Office', 'Data Mining']}]
+  
+
+  #setup extractor with base and decorators
+  decorators = {'Skill':ex.skills_dec , 'Language':ex.language_dec}
+  decorators = {'Skill':ex.skills_dec , 'Language':ex.language_dec}
+  extractor = ex.get_base
+  decorator = decorators["Language"]
+  extractor = decorator(extractor)
+
+  #retrieve files from static
+  resumesDir = os.listdir('static')
+  resumeFiles = []
+  resumes = []
+
+  for resumeFile in resumesDir:
+    if resumeFile.endswith(".pdf"):
+      resumeFiles.append('static/'+ resumeFile)
+  
+  for resumeFile in resumeFiles:
+    forExtractorInput = convertPDF.convertWithCoordinatesPara(resumeFile)
+    resume = extractor(forExtractorInput['pdfText'])
+    resume = "{" + resume +"}"
+    resumes.append(resume)
+
+  first = True
+  analyzerResumesInput = "["
+  for resume in resumes:
+    if first:
+      analyzerResumesInput = analyzerResumesInput + resume
+      first = False
+    else:
+      analyzerResumesInput = analyzerResumesInput + "," + resume
+
+  analyzerResumesInput = analyzerResumesInput + "]"
+  analyzerDictResumesInput = ast.literal_eval(analyzerResumesInput)
+  result = analyzer.process_analyzer(description, analyzerDictResumesInput)
   return json.dumps(result)
 
 @app.route('/')
@@ -71,11 +107,22 @@ def keyWordExtraction():
 def upload_file():
   if request.method == 'POST':
     upload_files = request.files.getlist("files")
+
+    #clear static directory
+    resumesDir = os.listdir('static')
+    resumeFiles = []
+    resumes = []
+    for resumeFile in resumesDir:
+      if resumeFile.endswith(".pdf"):
+        resumeFiles.append('static/'+ resumeFile)
+    for f in resumeFiles:
+      os.remove(f)
+
     for file in upload_files:
       filename = file.filename
       save_path = os.path.dirname(os.path.abspath(__file__))+'/static/'
       file.save(save_path+filename )
-      print json.dumps(convertPDF.convertWithCoordinatesPara('static/'+ filename))
+      #print json.dumps(convertPDF.convertWithCoordinatesPara('static/'+ filename))
   # # return jsonify({'status': 'created'}), 201
   
 if __name__ == '__main__':
