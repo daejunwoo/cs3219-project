0e9b3601fb2ed22873a745593bfececc253afd2b
daejunwoo <alan.tjw@gmail.com>
2015-11-09 21:15:53 +0800

    added json response, get form values

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ cors = CORS(app)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
+title_multiplier = 0
+skills_multiplier = 0
+more_multiplier = 0
+
 @app.route('/analyzer')
 def analyzeCV():
   description = {'Title': 'Software Engineer', 'Skill': ['Microsoft Office', 'Data Mining', 'Image Processing'], 'Certification': 'Random value', 'Volunteering': 'Random value'}
@@ -48,13 +52,16 @@ def keyWordExtraction():
 @cross_origin()
 def upload_file():
   if request.method == 'POST':
+    title_multiplier = request.form['title']
+    skills_multiplier = request.form['skills']
+    more_multiplier = request.form['more']
     upload_files = request.files.getlist("files")
     for file in upload_files:
       filename = file.filename
       save_path = os.path.dirname(os.path.abspath(__file__))+'/static/'
       file.save(save_path+filename )
       print json.dumps(convertPDF.convertWithCoordinatesPara('static/'+ filename))
-  # # return jsonify({'status': 'created'}), 201
+  return json.dumps({'status': '200'})
   
 if __name__ == '__main__':
   app.debug = True
